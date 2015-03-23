c9513e152218f2409e10ef63c420108d15052250
redpois0n <red@cmail.nu>
2015-03-23 22:02:10 +0100

    Standard classes added

---
+++ src/com/redpois0n/pathtree/PathListener.java
@@ -0,0 +1,7 @@
+package com.redpois0n.pathtree;
+
+public abstract interface PathListener {
+	
+	public abstract void pathSelected(String path);
+	
+}
--- src/pathtree/PathJTree.java
+++
@@ -1,5 +0,0 @@
-package pathtree;
-
-public class PathJTree {
-	
-}
