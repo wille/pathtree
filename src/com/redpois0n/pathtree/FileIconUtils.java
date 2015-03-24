ee463ca5d2defc078c96d97db873086ee0ddb181
redpois0n <red@cmail.nu>
2015-03-24 18:23:43 +0100

    Changes in FileIconUtils

--- src/com/redpois0n/pathtree/FileIconUtils.java
+++ src/com/redpois0n/pathtree/FileIconUtils.java
@@ -8,30 +8,28 @@ import javax.swing.filechooser.FileSystemView;
 
 public class FileIconUtils {
 	
-
-	public static Icon getFolderIcon() {
-		return getFileIconFromExtension("", true);
-	}
+	public static Icon getIconFromFile(File file) {
+		return FileSystemView.getFileSystemView().getSystemIcon(file);
+	}	
 	
-	public static Icon getFileIcon(File file) {
-		return getFileIconFromExtension(file.getAbsolutePath(), false);
+	public static Icon getFolderIcon() {
+		File temp = new File(System.getProperty("java.io.tmpdir") + File.separator + "icon");
+		temp.mkdirs();
+		
+		Icon icon = FileSystemView.getFileSystemView().getSystemIcon(temp);
+		
+		temp.delete();
+		
+		return icon;
 	}
 
-	public static Icon getFileIconFromExtension(String f, boolean dir) {
+	public static Icon getIconFromExtension(String f) {
 		try {
-			Icon icon;
-			
-			if (dir) {
-				File temp = new File(System.getProperty("java.io.tmpdir") + "icon");
-				temp.mkdirs();
-				icon = FileSystemView.getFileSystemView().getSystemIcon(temp);
-				temp.delete();
-			} else {
-				File temp = File.createTempFile((new Random()).nextInt() + "", f.substring(f.lastIndexOf("."), f.length()));
-				icon = FileSystemView.getFileSystemView().getSystemIcon(temp);
-				temp.delete();
-			}	
-			
+
+			File temp = File.createTempFile((new Random()).nextInt() + "", f.substring(f.lastIndexOf("."), f.length()));
+			Icon icon = FileSystemView.getFileSystemView().getSystemIcon(temp);
+			temp.delete();
+
 			return icon;
 		} catch (Exception ex) {
 			return null;
